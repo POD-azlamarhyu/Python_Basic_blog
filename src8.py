a8a72fd8c3fb866f10a5f8af084bdaa0f468fdf5
POD-azlamarhyu <adrague.ggoch@gmail.com>
2022-06-09 05:09:27 +0900

    first commit

---
+++ src8.py
@@ -0,0 +1,14 @@
+type_int = 1000
+alter_float = float(type_int)
+alter_string = str(type_int)
+print(alter_float)
+print(alter_string)
+
+type_float = 3.14159
+alter_int = int(type_float)
+alter_string = str(type_float)
+print(alter_int)
+print(alter_string)
+
+print("-----------------",end="\n\n")
+
